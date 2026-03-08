56327a654652c5199931bcfd1645d9f5c788861a
vardhman <142342717+vardh-man@users.noreply.github.com>
2026-03-08 06:09:05 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,65 +2,44 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-// var _ = net.Listen
-// var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
+
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment the code below to pass the first stage
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 
-	var conn net.Conn
-	conn, err = listener.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		handleConnection(conn)
+
 	}
 
-	var buffer []byte = make([]byte, 128)
-	var msg string = ""
+}
+
+func handleConnection(connection net.Conn) {
 	for {
-		n, err := conn.Read(buffer)
+		buf := make([]byte, 10)
+
+		_, err := connection.Read(buf)
 
 		if err != nil {
-			if err == io.EOF {
-				break
-			} else {
-				fmt.Println("Error Reading data from client", err)
-				os.Exit(1)
-			}
+			fmt.Println("There was an error reading client message", err)
+			os.Exit(1)
 		}
 
-		for i := range n {
-			msg += string(buffer[i])
-		}
-
-		// fmt.Println(msg)
-
-		// if msg == "PING" {
-		conn.Write([]byte("+PONG\r\n"))
-		// }
-
-		if len(msg) == 4 {
-			msg = ""
-		}
-
+		connection.Write([]byte("+PONG\r\n"))
 	}
-
-	// fmt.Println("Client disconnected from the server")
-
-	// conn.Write([]byte("+PONG\r\n"))
-
 }
